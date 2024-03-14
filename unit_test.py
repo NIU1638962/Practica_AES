aad7b1865cb2d6b4ccd7a919ff6c10ad89c279b1
Joel Tapia Salvador <91463596+NIU1638962@users.noreply.github.com>
2024-03-14 17:14:52 +0100

    Update 20240314c.

---
+++ unit_test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 16:48:37 2024
+
+@author: JoelT
+"""
+import unittest
+from unit_test_binary_polynomial import TestBinaryPolynomial
+
+if __name__ == "__main__":
+    unittest.main()
